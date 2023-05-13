5260f8fe35a9c7352e777c4715744193fc89ffcb
Jaroslav Künstler <109068586+kunstler-jaroslav@users.noreply.github.com>
2023-05-13 21:39:35 +0200

    Threading added
    Recording time can be set manually

--- aplikace/Speech_to_moves/VoiceContorl.py
+++ aplikace/Speech_to_moves/VoiceContorl.py
@@ -7,23 +7,60 @@
 
 import speech_recognition as sr
 import pyttsx3
+import time
+import threading
 
 
-class VoiceControl:
+class VoiceControl(threading.Thread):
 
-    def __init__(self, debug=False):
+    def __init__(self, debug=False, recordTime=5, info=True):
         """
         VoiceControl initialization
 
         :param debug: default False | enables prints for debug
+
+        :param recordTime: default 5 seconds | how long user input will be recorded each time
+
+        :param info: default True | print info for user
         """
+        threading.Thread.__init__(self)
         self.r = sr.Recognizer()
         self.engine = pyttsx3.init()
         self.debug = debug
         self.letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
         self.numbers = ["1", "2", "3", "4", "5", "6", "7", "8"]
+        self.data = ()
+        self.dataReady = False
+        self.runControl = True
+        self.recordTime = recordTime
+        self.info = info
 
-    def get_coordinates_times(self, max_times=2, info=True):
+    def run(self):
+        while self.runControl:
+            while not self.dataReady and self.runControl:
+                temp_data = self.get_coordinates_times(1)
+                if temp_data:
+                    self.data = temp_data
+                    self.dataReady = True
+                else:
+                    if self.info:
+                        print("Move not recognised from your command")
+
+    def read_data(self):
+        """
+        Returns recognised move from user voice command
+
+        :return: returns data or False if function called when data not ready
+        """
+        if self.dataReady:
+            self.dataReady = False
+            return self.data
+        else:
+            if self.debug:
+                print("VoiceControl.read_data called when data not ready")
+            return False
+
+    def get_coordinates_times(self, max_times=2):
         """
             COORDINATES ARE RETURNED IN ORDER AS THEY WERE SAID
 
@@ -31,74 +68,72 @@ class VoiceControl:
 
             :param max_times: how many times maximum the function tries to __listen() (user will be notifies each time
                                                                                         to speak if info = True)
-            :param info: True to print information for user, default True
             :return: tuple ((0, 0), (0, 1)) if found else False
         """
-        for i in range(max_times):                                          # listen for max_times
-            text = self.__listen(info)
-            if text:                                                        # User said something
-                num_cords = self.__analyze_text(text, info)
+        for i in range(max_times):  # listen for max_times
+            text = self.__listen()
+            if text:  # User said something
+                num_cords = self.__analyze_text(text)
                 if num_cords:
                     return num_cords
-        return False                                                        # not possible to get coordinates
+        return False  # not possible to get coordinates
 
-    def __analyze_text(self, text, info):
+    def __analyze_text(self, text):
         """
         Gets move coordinates from user text
 
         :param text: text from user in str format
-        :param info: default True, print information for user
+
         :return: tuple E.g. ((0, 0), (0, 1)) if found else False
         """
         coordinates = []
         numeric_coordinates = []
-        for l in range(len(self.letters)):                                  # try to find all combinations of numbers
-            for n in range(len(self.numbers)):                              # and letters from board
+        for l in range(len(self.letters)):  # try to find all combinations of numbers
+            for n in range(len(self.numbers)):  # and letters from board
                 c = self.letters[l] + self.numbers[n]
                 if c in text:
                     coordinates.append(c)
                     tup = (l, n)
                     numeric_coordinates.append(tup)
-        if len(coordinates) == 2:                                           # two coordinates were found, firs said
-            if text.find(coordinates[0]) > text.find(coordinates[1]):       # first in tuple
+        if len(coordinates) == 2:  # two coordinates were found, firs said
+            if text.find(coordinates[0]) > text.find(coordinates[1]):  # first in tuple
                 coordinates[0], coordinates[1] = coordinates[1], coordinates[0]
                 numeric_coordinates[0], numeric_coordinates[1] = numeric_coordinates[1], numeric_coordinates[0]
             board_cords = (coordinates[0].upper(), coordinates[1].upper())
             num_cords = (numeric_coordinates[0], numeric_coordinates[1])
-            if info:
+            if self.info:
                 print("VoiceControl recognised move {}".format(board_cords))
             if self.debug:
                 print("VoiceControl recognised move {}".format(num_cords))
             return num_cords
         return False
 
-    def __listen(self, info=True):
+    def __listen(self):
         """
         Listens for user input once (takes about 5 seconds)
 
-        :param info: default True | print info for user
         :return: str format of user spoken input
         """
         try:
             with sr.Microphone() as source:
-                if info:
+                if self.info:
                     print("You can speak in a second")
-                self.r.adjust_for_ambient_noise(source, duration=1)         # Surroundings adjustment
-                if info:
+                self.r.adjust_for_ambient_noise(source, duration=0.4)  # Surroundings adjustment
+                if self.info:
                     print("You can speak")
 
-                audio = self.r.listen(source)                               # Listen for user input
+                audio = self.r.record(source, duration=self.recordTime)  # Listen for user input
 
-                text = self.r.recognize_google(audio).lower()               # Google text recognition
+                text = self.r.recognize_google(audio).lower()  # Google text recognition
 
                 if self.debug:
                     print("Did you say ", text)
                 return text
 
-        except sr.RequestError as e:                                        # Server connection error
+        except sr.RequestError as e:  # Server connection error
             print("ERROR: VoiceControl - request not possible; {0}".format(e))
 
-        except sr.UnknownValueError:                                        # Anything else
+        except sr.UnknownValueError:  # Anything else
             print("ERROR: VoiceControl - unknown")
 
     def SpeakText(self, command, speed=150):
@@ -114,11 +149,22 @@ class VoiceControl:
 
 
 if __name__ == "__main__":
-    voice_control = VoiceControl(debug=False)
     # commands in english, can be said anything containing the two coordinates FIRST where we start SECOND where we go
     # E.g. move this from A3 to maybe A4, will return ((0, 2), (0, 3))
-    # 
-    move_num = voice_control.get_coordinates_times(3, info=True)
-    print(move_num)
-    voice_control.SpeakText("Text to be said.", speed=150)
+    voice_control = VoiceControl(debug=False, info=True, recordTime=4)  # initialize
+    voice_control.start()
 
+    # sample code
+    cnt = 0
+    while True:
+        if cnt >= 2:
+            print("quiting")
+            voice_control.runControl = False
+            voice_control.join(1)
+            exit()
+        if voice_control.dataReady:
+            # use voice_control.read_data() to get the loaded data NOT voice_control.data
+            move_num = voice_control.read_data()
+            print(move_num)
+            # voice_control.SpeakText(str(move_num))     # talks to user
+            cnt += 1
