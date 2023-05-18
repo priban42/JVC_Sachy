aeef342f2944c20a61ee18fa6f507ad41496f694
styblto1 <126575018+styblto1@users.noreply.github.com>
2023-05-18 15:21:38 +0200

    Update moves_to_commands.py

--- aplikace/Moves_to_commands/moves_to_commands.py
+++ aplikace/Moves_to_commands/moves_to_commands.py
@@ -15,29 +15,45 @@ class MoveToCmds:
     self.shiftL = 0.5
   
   def update_cords(self, cords):
+    '''
+    Function updates start and final cords if it's called.
+    '''
     self.start_cords = Cords(cords[0][0], cords[0][1])
     self.final_cords = Cords(cords[1][0], cords[1][1])
   
   def fill_board_status(self):
+    '''
+    Function fill chess status with cords as a key and assigns it state - 0 - there is nothing on this position.
+    It returns dictionary with cords as a key and value 0.
+                                                                      
+    '''
     for i in np.arange(0,7.5,self.shiftL):
      for j in np.arange(0,7.5,self.shiftL):
         self.chess_status[(i,j)] = 0
     return self.chess_status
 
   def update_board(self, board):
+    '''
+    Function clears board status and adds 1 on positions, where figures are, by given array of positions (board) of figures.
+    It returns dictionary with cords as a key and values 0 or 1.
+    '''
     self.fill_board_status()
     for i,j in board:
       self.chess_status[(i,j)] = 1
     return self.chess_status
 
   def make_path_lines(self, path, crds):
+    '''
+    At the start its decide, wich line is closer to final position, than the sign is set.
+    Function goes by lines on board till it reaches same row, where final postion is.
+    Then it goes in columns until it reaches same column. In the end it goes up or down
+    base on decision, that was made at the beginning.
+    It returns array of cords (x,y).
+    '''
     xs, ys = crds
     xf, yf = self.final_cords.x, self.final_cords.y
     sign = ''
-    '''
-    Rozhodne jakým směrem půjde na základě vzdálenosti 
-    finálních souřadnic a startovních souřadnic
-    '''
+    
     if xs >= xf:
       if xs - self.shiftL >= 0:
         xs -= self.shiftL
@@ -57,27 +73,21 @@ class MoveToCmds:
           ys -= self.shiftL
           sign = '-'
     path.append((xs,ys))
-    '''
-    Pohyb po svislých krajích šachovnice
-    '''
+   
     while ys != yf - self.shiftL and ys != yf + self.shiftL:
       if ys < yf:
         ys += self.shiftL
       else:
         ys -= self.shiftL
       path.append((xs,ys))
-    '''
-    Pohyb po horizontálních krajích
-    '''
+   
     while xs != xf:
       if xs < xf:
         xs += self.shiftL
       else:
         xs -= self.shiftL
       path.append((xs,ys))
-    '''
-    Na základně předchozího znaménka rozhodne zda je cíl nahoře nebo dole
-    '''
+  
     if sign == '+':
       path.append((xs,ys+self.shiftL))
     else:
@@ -85,15 +95,18 @@ class MoveToCmds:
     return True, path
 
   def make_path_squares(self):
+    '''
+    Function goes by squares until it reaches final cords or the square on cords (xs,ys) is occupied.
+    If it's occupied then it switches to another function that goes by lines.
+    It returns validity of path, array of cords (x,y) and last cords.
+    '''
     xs, ys = self.start_cords.x, self.start_cords.y
     xf, yf = self.final_cords.x, self.final_cords.y
     xbf = 0
     ybf = 0
     path = []
     path.append((xs,ys))
-    '''
-    Pohyb po čtverečkách pokud nemá nic ve svém řádku/sloupci
-    '''
+   
     while xs != xf or ys != yf:
       xbf = xs
       if xs < xf and xs + self.shiftL <= 7:
@@ -114,6 +127,10 @@ class MoveToCmds:
     return True, path, (xs,ys)
 
   def decide(self):
+    '''
+    This function decide, what function does make the path.
+    It returns array of cords (x,y).
+    '''
     valid, path, crds = self.make_path_squares()
     if valid:
       print(path)
@@ -128,40 +145,17 @@ class MoveToCmds:
         exit()
 
   def move(self, board, cords):
+    '''
+    Main function, calls other functions.
+    Returns array of cords (x,y).
+    '''
     self.update_cords(cords)
     self.update_board(board)
     return self.decide()
 
-  def print_board(self, path):
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_axes([0.05, 0.05, 1, 1])
-    ax.set_xlim(-1, 8)
-    ax.set_ylim(-1, 8)
-    for j in np.arange(0,7.5,0.5):
-      for i in np.arange(0,7.5,0.5):
-        if (i,j) in path:
-          c = 'blue'
-          s = 100
-        elif i%1 == 0 and j%1 == 0 and self.chess_status[(i,j)] != 0:
-          c = 'red'
-          s = 100
-        elif  i%1 == 0 and j%1 == 0:
-          c = 'green'
-          s = 100
-        else:
-          c = 'black'
-          s = 10
-        ax.scatter(i,j, s=s, c=c, marker='o')
-    plt.show()
-
-
 if __name__ == "__main__":
   move = MoveToCmds()
-  i = 0
-  while i < 1:
-    if i == 0:
-      board = [(1,1),(2,2),(6,5)]
-      cords = [(6,0),(3.5,3.5)]
-    move.print_board(move.move(board,cords))
-    i += 1
+  board = [(1,1),(2,2),(6,5)]
+  cords = [(6,0),(3.5,3.5)]
+  move.move(board,cords)
   
